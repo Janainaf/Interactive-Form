bf77158369c8e7e8534bd93b9ce98d4c4ef8c5c9
Janainaf <figueira.janaina@gmail.com>
2021-07-16 19:06:40 +0200

    some refactoring and a bit of payment

--- js/script.js
+++ js/script.js
@@ -21,38 +21,44 @@ document.getElementById("shirt-colors").style.display = "none";
 document.getElementById("design").onchange = function () {
   document.getElementById("shirt-colors").style.display = "block";
 
+  Array.from(document.querySelectorAll("[data-theme")).forEach(function (them) {
+    them.style.display = "none";
+  });
+
   if (document.getElementById("design").value == "heart js") {
-    var tshirtsPun = document.querySelectorAll("[data-theme='js puns']");
-    for (let tPun of tshirtsPun) {
-      tPun.style.display = "none";
-    }
-    var tshirtHeart = document.querySelectorAll("[data-theme='heart js']");
-    for (let tHeart of tshirtHeart) {
-      tHeart.style.display = "block";
-    }
+    Array.from(
+      document
+        .querySelectorAll("[data-theme='heart js']")
+        .forEach(function (them) {
+          them.style.display = "block";
+        })
+    );
   }
 
   if (document.getElementById("design").value == "js puns") {
-    var tshirtHeart = document.querySelectorAll("[data-theme='heart js']");
-    for (let tHeart of tshirtHeart) {
-      tHeart.style.display = "none";
-    }
-    var tshirtsPun = document.querySelectorAll("[data-theme='js puns']");
-    for (let tPun of tshirtsPun) {
-      tPun.style.display = "block";
-    }
+    Array.from(
+      document
+        .querySelectorAll("[data-theme='js puns']")
+        .forEach(function (them) {
+          them.style.display = "block";
+        })
+    );
   }
 };
-
 // *******************  Total*******************
-document.getElementById("activities-box").onchange = function () {
-  var boxActivity = document.querySelectorAll(".activity-cost");
-  for (let activity of boxActivity) {
-    if (activity !== null) {
-      console.log(activity.textContent);
-    }
-  }
-};
+
+var checkBoxes = document.querySelectorAll("input[type=checkbox]");
+let checkedBoxes = [];
+
+checkBoxes.forEach(function (checkbox) {
+  checkbox.addEventListener("change", function () {
+    checkedBoxes = Array.from(checkBoxes)
+      .filter((i) => i.checked)
+      .map((i) => i.dataset.cost);
+
+    console.log(checkedBoxes);
+  });
+});
 
 // ********************* "Payment Info" section *******************
 
@@ -65,3 +71,5 @@ function selectElement(id, valueToSelect) {
 
 document.getElementById("paypal").style.display = "none";
 document.getElementById("bitcoin").style.display = "none";
+
+// ********************* "Payment Info" section *******************
