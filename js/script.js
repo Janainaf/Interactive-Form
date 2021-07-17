b9e1a8d2360516e87f790ccd5ff2c72bd75498e4
Janainaf <figueira.janaina@gmail.com>
2021-07-17 10:34:35 +0200

    chekbox bug fixed. Next step, finish payment, validation and refactoring

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log("Test");
 document.getElementById("name").focus();
 
 //*******************  Job Title *******************
-// Reminder:  visibility and display are different and dont behave the same
+// Reminder:  visibility and display are different and don't behave the same
 document.getElementById("other-job-role").style.display = "none";
 
 document.getElementById("title").onchange = function () {
@@ -15,61 +15,56 @@ document.getElementById("title").onchange = function () {
 };
 
 //*******************  Tshirt *******************
-// needs refactoring
-document.getElementById("shirt-colors").style.display = "none";
+// needs SERIOUSLY refactoring
 
+document.getElementById("shirt-colors").style.display = "none";
 document.getElementById("design").onchange = function () {
   document.getElementById("shirt-colors").style.display = "block";
 
-  Array.from(document.querySelectorAll("[data-theme")).forEach(function (them) {
-    them.style.display = "none";
+  Array.from(document.querySelectorAll("[data-theme]")).forEach(function (tee) {
+    tee.style.display = "none";
   });
 
   if (document.getElementById("design").value == "heart js") {
-    Array.from(
-      document
-        .querySelectorAll("[data-theme='heart js']")
-        .forEach(function (them) {
-          them.style.display = "block";
-        })
-    );
+    document
+      .querySelectorAll("[data-theme='heart js']")
+      .forEach(function (heartTee) {
+        heartTee.style.display = "block";
+      });
   }
 
   if (document.getElementById("design").value == "js puns") {
-    Array.from(
-      document
-        .querySelectorAll("[data-theme='js puns']")
-        .forEach(function (them) {
-          them.style.display = "block";
-        })
-    );
+    document
+      .querySelectorAll("[data-theme='js puns']")
+      .forEach(function (punTee) {
+        punTee.style.display = "block";
+      });
   }
 };
 // *******************  Total*******************
 
 var checkBoxes = document.querySelectorAll("input[type=checkbox]");
-let checkedBoxes = [];
+let sum = 0;
 
 checkBoxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
-    checkedBoxes = Array.from(checkBoxes)
-      .filter((i) => i.checked)
-      .map((i) => i.dataset.cost);
-
-    console.log(checkedBoxes);
+    var box = parseInt(checkbox.dataset.cost) * (checkbox.checked ? 1 : -1);
+    sum += box;
+    document.getElementById("activities-cost").innerHTML = "Total: $" + sum;
   });
 });
 
 // ********************* "Payment Info" section *******************
+//need to finish it - use select to loop through option values... like the active button
+// set credit card default selected
 
 selectElement("payment", "credit-card");
+document.getElementById("paypal").style.display = "none";
+document.getElementById("bitcoin").style.display = "none";
 
 function selectElement(id, valueToSelect) {
   let element = document.getElementById(id);
   element.value = valueToSelect;
 }
 
-document.getElementById("paypal").style.display = "none";
-document.getElementById("bitcoin").style.display = "none";
-
-// ********************* "Payment Info" section *******************
+// ********************* Form validation *******************
