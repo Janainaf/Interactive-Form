86deea3a02450cb20859c8a687a278a9c4216fa7
Janainaf <figueira.janaina@gmail.com>
2021-10-26 15:11:28 +0200

    refactoring and cleaning code

--- js/script.js
+++ js/script.js
@@ -1,32 +1,53 @@
-console.log("Test");
+const form = document.querySelector("form");
+const nameElement = document.getElementById("name");
+const email = document.getElementById("email");
+const creditCard = document.getElementById("cc-num");
+const languageTotalElement = document.querySelector("#language-total");
 
-document.getElementById("name").focus();
+window.onload = function () {
+  checkBoxes.forEach(function (checkbox) {
+    checkbox.checked = false;
+    nameElement.value = "";
+    email.value = "";
+    creditCard.value = "";
+  });
+};
+
+//*******************  Focus on Name *******************
+
+nameElement.focus();
 
 //*******************  Job Title *******************
 // Reminder:  visibility and display are different and don't behave the same
-document.getElementById("other-job-role").style.display = "none";
 
-document.getElementById("title").onchange = function () {
-  if (document.getElementById("title").value != "other") {
-    document.getElementById("other-job-role").style.display = "none";
+const jobTitle = document.getElementById("title");
+const otherJob = document.getElementById("other-job-role");
+
+otherJob.style.display = "none";
+jobTitle.onchange = function () {
+  if (jobTitle.value != "other") {
+    otherJob.style.display = "none";
   } else {
-    document.getElementById("other-job-role").style.display = "block";
+    otherJob.style.display = "block";
   }
 };
 
 //*******************  Tshirt *******************
 // needs SERIOUSLY refactoring
 
-document.getElementById("shirt-colors").style.display = "none";
+const shirtColors = document.getElementById("shirt-colors");
+const shirtDesign = document.getElementById("design");
 
-document.getElementById("design").onchange = function () {
-  document.getElementById("shirt-colors").style.display = "block";
+shirtColors.style.display = "none";
+
+shirtDesign.onchange = function () {
+  shirtColors.style.display = "block";
 
   Array.from(document.querySelectorAll("[data-theme]")).forEach(function (tee) {
     tee.style.display = "none";
   });
 
-  if (document.getElementById("design").value == "heart js") {
+  if (shirtDesign.value == "heart js") {
     document
       .querySelectorAll("[data-theme='heart js']")
       .forEach(function (heartTee) {
@@ -34,7 +55,7 @@ document.getElementById("design").onchange = function () {
       });
   }
 
-  if (document.getElementById("design").value == "js puns") {
+  if (shirtDesign.value == "js puns") {
     document
       .querySelectorAll("[data-theme='js puns']")
       .forEach(function (punTee) {
@@ -46,9 +67,6 @@ document.getElementById("design").onchange = function () {
 
 var checkBoxes = document.querySelectorAll("input[type=checkbox]");
 let sum = 0;
-if (sum < 0) {
-  sum = 0;
-}
 
 checkBoxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
@@ -56,14 +74,9 @@ checkBoxes.forEach(function (checkbox) {
     sum += box;
     document.getElementById("activities-cost").innerHTML = "Total: $" + sum;
   });
-  window.onload = function () {
-    // console.log("try to checkbox.checked = false");
-    checkbox.checked = false;
-  };
 });
 
 // ********************* "Payment Info" section *******************
-// set credit card default selected
 
 function selectElement(id, valueToSelect) {
   let element = document.getElementById(id);
@@ -79,31 +92,67 @@ var paymentMethods = ["credit-card", "paypal", "bitcoin"];
 document.getElementById("payment").onchange = function () {
   for (var i = 0; i < paymentMethods.length; i++) {
     document.getElementById(paymentMethods[i]).style.display = "none";
-    console.log(paymentMethods[i]);
   }
   var payIndex = document.getElementById("payment").selectedIndex;
-  console.log(paymentMethods[payIndex]);
   document.getElementById(paymentMethods[payIndex - 1]).style.display = "block";
 };
 
 // ********************* Form validation *******************
-document.querySelector("form").onsubmit = function () {
+
+document.querySelector("form").onsubmit = function (e) {
+  e.preventDefault();
+  // Only call `preventDefault` on the `event` object if one or more of the required fields is invalid.
+
   var regName = /(.|\s)*\S(.|\s)*/;
-  var name = document.getElementById("name");
-  if (regName.test(name.value) == false) {
-    alert("Invalid Name");
-    name.focus();
+  if (regName.test(nameElement.value) == false) {
+    document.getElementById("name-hint").style.display = "block";
+    nameElement.parentElement.classList.add(".not-valid");
+    nameElement.focus();
     return false;
+  } else {
+    nameElement.parentElement.classList.add(".valid");
   }
-  var regEmail = /(.|\s)*\S(.|\s)*/;
-  var email = document.getElementById("email");
+
+  var regEmail = /.+\@.+\.(com)/;
   if (regEmail.test(email.value) == false) {
-    alert("Invalid Email");
+    document.getElementById("email-hint").style.display = "block";
+    email.parentElement.classList.add(".not-valid");
     email.focus();
     return false;
   }
+
   if (sum == 0) {
-    alert("Error: Check a box!");
+    document.getElementById("activities-hint").style.display = "block";
+    // xxx.parentElement.classList.add(".not-valid");
     return false;
   }
+
+  // If and only if credit card is the selected payment method:
+
+  if (document.getElementById("payment").selectedIndex === 1) {
+    var regCardNumber = /(.|\s)*\S(.|\s)*/;
+
+    // The "Card number" field must contain a 13 - 16 digit credit card number with no dashes or spaces. The value does not need to be a real credit card number.
+
+    if (regCardNumber.test(creditCard.value) == false) {
+      document.getElementById("cc-hint").style.display = "block";
+      return false;
+    }
+
+    // The "Zip code" field must contain a 5 digit number.
+  }
+  // The "CVV" field must contain a 3 digit number.
 };
+
+// ********************* Accessibility *******************
+
+[...document.querySelectorAll("input[type=checkbox]")].forEach((course) => {
+  course.addEventListener("focus", (e) =>
+    course.parentElement.classList.add("focus")
+  );
+  course.addEventListener("blur", (e) => {
+    console.log("focusin!");
+    const active = document.querySelector(".focus");
+    if (active) active.classList.remove("focus");
+  });
+});
